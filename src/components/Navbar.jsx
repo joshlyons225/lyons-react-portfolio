1a1c1c7aff741773e491098fa826fde6ec07e651
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-14 02:32:44 -0500

    added navbar ul/li

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,46 @@
 // import files and dependencies
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { GiLetterBomb } from "react-icons/gi";
+import { GrDocumentText } from "react-icons/gr";
+import { Link } from "react-scroll";
+// placeholder for resume; import Resume from "../assets"
 
 // create Navbar section function
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const clickHandler = () => setNav(!nav);
+
   return (
-    <div>
-      <h1>TEST</h1>
-    </div>
+    <section className="fixed w-full h-[200px] flex justify-between items-center px-4 bg-slate-600 text-stone-600">
+      <ul className="hidden md:flex">
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="projects" smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </section>
   );
 };
 
