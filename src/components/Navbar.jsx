6a00c3584a3f35b862d03cdac2d1b38a84ee7323
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-14 02:41:08 -0500

    attempted to create hidden nav contents

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,6 +40,53 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+
+      <div onClick={clickHandler} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={clickHandler} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={clickHandler} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={clickHandler} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link onClick={clickHandler} to="work" smooth={true} duration={500}>
+            Projects
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <Link
+            onClick={clickHandler}
+            to="contact"
+            smooth={true}
+            duration={500}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
     </section>
   );
 };
