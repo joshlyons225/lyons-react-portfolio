1227aeb0c44655d18e1074e2013e056535cf1a1d
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-14 02:54:00 -0500

    added socials toggler

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,6 +87,47 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
+
+      {/* Little socials thingy */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href=""
+              target={"_blank"}
+            >
+              <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/joshlyons225"
+              target={"_blank"}
+            >
+              <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#aea3b0]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:joshlyons225@gmail.com"
+            >
+              <GiLetterBomb size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href=""
+              target={"_blank"}
+            >
+              <GrDocumentText size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 };
