2fec1100d486af7c8a5f37edbcc63ae16ac5741e
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-14 21:11:58 -0500

    commented out jump to projects div in Home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 // import files and dependencies
 import React from "react";
-import { HiArrowNarrowRight } from "react-icons/hi";
+import { TiArrowDownThick } from "react-icons/ti";
 import { Link } from "react-scroll";
 
 // create Home section function
@@ -10,16 +10,16 @@ const Home = () => {
       name="home"
       className="w-full h-screen text-stone-600 bg-slate-800"
     >
-      <div>
-        <Link to="work" smooth={true} duration={500}>
+      {/* <div>
+        <Link to="projects" smooth={true} duration={500}>
           <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-900 hover:border-purple-600">
-            View Work
+            Jump to Projects
             <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3 " />
+              <TiArrowDownThick className="ml-3 " />
             </span>
           </button>
         </Link>
-      </div>
+      </div> */}
 
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <h1 className="text-7xl font-bold">Josh Lyons</h1>
