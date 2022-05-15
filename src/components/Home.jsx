5e1a87f878da421355b2d192af6d81c2a8ef0a88
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-05-15 18:51:59 -0500

    updated Home section to eliminate jump tp projects button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,5 @@
 // import files and dependencies
 import React from "react";
-// import { TiArrowDownThick } from "react-icons/ti";
-// import { Link } from "react-scroll";
 
 // create Home section function
 const Home = () => {
@@ -10,17 +8,6 @@ const Home = () => {
       name="home"
       className="w-full h-screen text-stone-600 bg-slate-800"
     >
-      {/* <div>
-        <Link to="projects" smooth={true} duration={500}>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-900 hover:border-purple-600">
-            Jump to Projects
-            <span className="group-hover:rotate-90 duration-300">
-              <TiArrowDownThick className="ml-3 " />
-            </span>
-          </button>
-        </Link>
-      </div> */}
-
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <h1 className="text-7xl font-bold">Josh Lyons</h1>
         <h2 className="text-5xl font-bold">Full Stack Developer</h2>
